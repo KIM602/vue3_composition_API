51e1fa640585a6ab0ee4e2dc0e06eda1605d8273
KIM602 <kyk940608@gmail.com>
2023-12-26 17:05:01 +0900

    기본 옵션과 라이프사이클

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
     <h1 @click="increase">
-        {{ count }}
+        {{ count }} / {{ doubleCount }}
+    </h1>
+    <h1 @click="changeMessage">
+        {{ message }} / {{ doubleCount }}
     </h1>
 </template>
 
 <script>
-import { ref } from 'vue'
 export default {
-    setup() {
-        let count = ref(0)
-        function increase() {
-            count.value += 1
-        }
-
+    data() {
         return {
-            count,
-            increase
+            count: 0,
+            message: 'Hello World!'
         }
-    }
+    },
+    computed: {
+        doubleCount() {
+            return this.count * 2
+        },
+        reversedMessage() {
+            return this.message.split('').reverse().join('')
+        }
+    },
+    watch: {
+        message(newValue) {
+            console.log(newValue)
+        }
+    },
+    created() {
+        console.log(this.message)
+    },
+    mounted() {
+        console.log(this.count)
+    },
+    methods: {
+        increase() {
+            this.count += 1
+        },
+        changeMessage() {
+            this.message = 'Good?!'
+        }
+    },
 }
 </script>
